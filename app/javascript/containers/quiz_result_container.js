0cf06db71dbee4db2a534dadd81711084cdbfab3
r.nishio <quick.xp@gmail.com>
2018-05-06 00:51:31 +0900

    dev

---
+++ app/javascript/containers/quiz_result_container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux'
+import QuizComponent from '../components/quiz_component';
+
+const mapStateToProps = (state) => {
+  return {
+  };
+}
+
+const mapDispatchToProps = dispatch => ({
+  });
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizResultComponent);
---
+++ app/javascript/packs/quiz_result.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '../stores/app_store';
+import QuizResultContainer from '../containers/quiz_result_container';
+
+const appStore = configureStore();
+
+class QuizResultsApp extends React.Component {
+  render() {
+    const { store } = this.props;
+
+    return (
+      <Provider store={store}>
+        <QuizResultContainer/>
+      </Provider>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const serverNode = document.getElementById('server-side-contents');
+  const serverData = JSON.parse(serverNode.innerText);
+  window.serverData = serverData;
+  const node = document.getElementById('component-quiz-result');
+  if (node) {
+    ReactDOM.render(<QuizResultApp store={appStore} {...serverData} />, node);
+  }
+});
