eaf56947f62779d23b11d7bebe2490ff16af7eb1
r.nishio <quick.xp@gmail.com>
2018-05-13 14:52:51 +0900

    dev

--- db/schema/schema.rb
+++ db/schema/schema.rb
@@ -10,3 +10,24 @@ create_table "ar_internal_metadata", collate: "utf8_bin", comment: "" do |t|
   t.datetime :created_at
   t.datetime :updated_at
 end
+
+create_table "questions", collate: "utf8_bin", comment: "" do |t|
+  t.int :id, primary_key: true, extra: :auto_increment
+  t.text :question
+  t.text :hint, null: true
+  t.text :comment, null: true
+  t.int :status, default: 0
+  t.int :created_by, null: true
+
+  t.datetime :created_at
+  t.datetime :updated_at
+end
+
+create_table "choices", collate: "utf8_bin", comment: "" do |t|
+  t.int :id, primary_key: true, extra: :auto_increment
+  t.int :question_id
+  t.varchar :choice
+
+  t.datetime :created_at
+  t.datetime :updated_at
+end
