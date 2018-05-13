dfab0e0009f1f471a2a643013ded80d8b4a9a8b1
r.nishio <quick.xp@gmail.com>
2018-05-13 17:15:21 +0900

    dev

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,20 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  question   :text(65535)      not null
+#  hint       :text(65535)
+#  comment    :text(65535)
+#  status     :integer          default(0), not null
+#  created_by :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Question < ActiveRecord::Base
   acts_as_taggable # Alias for acts_as_taggable_on :tags
   acts_as_taggable_on :categories
+
+  has_many :choices
 end
