d13d301721a9d3957782048ab889c460c091a59f
r.nishio <quick.xp@gmail.com>
2018-05-26 15:05:11 +0900

    dev

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   get 'quiz_home/index'
-
   get 'quiz_results/index'
-
-  get 'pages/index'
+  get 'quiz/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
