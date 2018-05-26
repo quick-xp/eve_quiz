c10a1f70d13ce951db32340d68a4e094408f3e7c
r.nishio <quick.xp@gmail.com>
2018-05-26 17:01:15 +0900

    dev

--- app/javascript/actions/quiz.js
+++ app/javascript/actions/quiz.js
@@ -16,3 +16,37 @@ export function setQuizLists(historyId) {
     historyId
   };
 }
+
+export function fetchQuizRequest() {
+  return {
+    type: FETCH_QUIZ_REQUEST
+  };
+}
+
+export function fetchQuizSuccess(results) {
+  return {
+    type: FETCH_QUIZ_SUCCESS,
+    results
+  };
+}
+
+export function fetchQuizFail(error) {
+  return {
+    type: FETCH_QUIZ_FAIL,
+    error
+  };
+}
+
+export function fetchQuiz(seasonSlug) {
+  return (dispatch, getState) => {
+    dispatch(fetchQuizRequest());
+    api(getState)
+      .get(`/api/seasons/${seasonSlug}/ranking_makers/edit`)
+      .then(response => {
+        dispatch(fetchQuizSuccess(response.data));
+      })
+      .catch(error => {
+        dispatch(fetchQuizFail(error));
+      });
+  };
+}
